e60da89cc883eee35a50436637ee168115a971cb
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-06 21:35:23 -0300

    Primeiro Commit

---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace UnclePhill.WebAPI_NFeS.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace UnclePhill.WebAPI_NFeS.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
