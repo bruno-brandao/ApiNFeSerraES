3623c15b684ff9777eeca76bdbfd2c6d8d6f8808
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-07 00:04:51 -0300

    Estruturado o projeto

---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.DataAcess.Entity/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnclePhill.WebAPI_NFeS.DataAcess.Entity
+{
+    public class Class1
+    {
+    }
+}
