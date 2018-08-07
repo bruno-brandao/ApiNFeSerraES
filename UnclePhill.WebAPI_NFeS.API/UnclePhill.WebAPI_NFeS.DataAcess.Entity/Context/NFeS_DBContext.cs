efc6d6f5cfbfdcd59a2fca35c368ab0839800bab
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-07 00:22:12 -0300

    Criando as camadas ada API

---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.DataAcess.Entity/Context/NFeS_DBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnclePhill.WebAPI_NFeS.Domain;
+
+namespace UnclePhill.WebAPI_NFeS.DataAcess.Entity.Context
+{
+    public class NFeS_DBContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public NFeS_DBContext()
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
