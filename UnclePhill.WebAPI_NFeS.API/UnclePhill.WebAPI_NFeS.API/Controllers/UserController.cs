c7e3818e36685b3210707c10c30f256b0cf98c9f
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-08 01:08:39 -0300

    Construindo a primeira controller para teste

---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UnclePhill.Common.Repository.Interface;
+using UnclePhill.WebAPI_NFeS.DataAcess.Entity.Context;
+using UnclePhill.WebAPI_NFeS.Domain;
+using UnclePhill.WebAPI_NFeS.Repository.Entity;
+
+namespace UnclePhill.WebAPI_NFeS.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        private IRepositoryUnclePhill<User, long> _repositoryUser 
+            = new RepositoryUser(new NFeS_DBContext());
+
+        public IEnumerable<User> Get()
+        {
+            return _repositoryUser.Select();
+        }
+
+        public User Get(long? id)
+        {
+            return _repositoryUser.SelectByKey(id.Value);
+        }
+    }
+}
