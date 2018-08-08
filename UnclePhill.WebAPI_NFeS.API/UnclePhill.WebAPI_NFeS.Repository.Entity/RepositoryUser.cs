305fd590e4811275b06e70ab2a734c60ea7ee166
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-08 00:36:39 -0300

    Criando estrutura do repositorio da API

---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Repository.Entity/RepositoryUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnclePhill.Common.Repository.Entity;
+using UnclePhill.WebAPI_NFeS.DataAcess.Entity.Context;
+using UnclePhill.WebAPI_NFeS.Domain;
+
+namespace UnclePhill.WebAPI_NFeS.Repository.Entity
+{
+    public class RepositoryUser: RepositoryUnclePhill<User,long>
+    {
+        public RepositoryUser(NFeS_DBContext context) : base(context)
+        {
+
+        }
+    }
+}
