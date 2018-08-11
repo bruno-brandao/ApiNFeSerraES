4306aac0114dc37c2ede17f5d4e3a6925efb39ca
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-10 23:55:53 -0300

    Teste de metodos

--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Controllers/UserController.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Controllers/UserController.cs
@@ -14,9 +14,21 @@ namespace UnclePhill.WebAPI_NFeS.API.Controllers
 
         private BD_NFeS Instace = new BD_NFeS();
 
-        public List<Users> Get()
+        public List<Users> Post()
         {
             return Instace.Users.ToList();
+        }
+
+        public Users Post(int? Id)
+        {
+            return Instace.Users.Where(Func => Func.UserId == Id).First();
+        }
+
+        public bool Put(Users users)
+        {
+            Instace.Users.Add(users);
+            Instace.SaveChanges();
+            return true;
         }
     }
 }
