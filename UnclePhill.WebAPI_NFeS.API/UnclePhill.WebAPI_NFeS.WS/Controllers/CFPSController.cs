6382b21b218587394bf3697293933260c51e0a6f
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-08-29 19:22:29 -0300

    Vai

--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.WS/Controllers/CFPSController.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.WS/Controllers/CFPSController.cs
@@ -66,8 +66,8 @@ namespace UnclePhill.WebAPI_NFeS.WS.Controllers
                 WSEntrada.consultarAtividadesRequest Request = new WSEntrada.consultarAtividadesRequest();
                 Request.Body = new WSEntrada.consultarAtividadesRequestBody();
                 Request.Body.cpfUsuario = "555.555.555-55";
-                Request.Body.hashSenha = "cRDtpNCeBiql5KOQsKVyrA0sAiA=";
                 Request.Body.inscricaoMunicipal = "99999";
+                Request.Body.hashSenha = "cRDtpNCeBiql5KOQsKVyrA0sAiA=";                
                 Request.Body.codigoMunicipio = 3;        
 
                 WSEntrada.consultarAtividadesResponse ApiResponse = Entrada.consultarAtividades(Request);
