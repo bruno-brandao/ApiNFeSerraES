306e65315f008498dce8c33f43446f242d900fa7
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-09-05 21:50:41 -0300

    Vai

--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/App_Start/WebApiConfig.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/App_Start/WebApiConfig.cs
@@ -17,7 +17,7 @@ namespace UnclePhill.WebAPI_NFeS.API
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { id = RouteParameter.Optional } 
             );
         }
     }
