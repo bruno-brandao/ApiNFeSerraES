1c7b508553885fc9601f031de99b2b5eadb63b0d
Everaldo Cardoso de Araujo <everaldocardoso.github@gmail.com>
2018-09-10 10:58:54 -0300

    ss

--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Domain/Domain/NFeSDomain.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Domain/Domain/NFeSDomain.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Xml.Serialization;
 using UnclePhill.WebAPI_NFeS.Domain.NFeS.API.Serra.Entrada;
-using UnclePhill.WebAPI_NFeS.Models.Models.NFeSStructure.NFeSIssueRequest;
 
 namespace UnclePhill.WebAPI_NFeS.Domain
 {
@@ -26,63 +20,61 @@ namespace UnclePhill.WebAPI_NFeS.Domain
                 NFeSIR.nfd.razaotomador = "SMARapd - ltda";
                 NFeSIR.nfd.nomefantasiatomador = "SMARapd";
                 NFeSIR.nfd.enderecotomador = "Rua Aurora";
-                NFeSIR.nfd.numeroendereco = 446;
                 NFeSIR.nfd.cidadetomador = "Ribeirão Preto";
                 NFeSIR.nfd.estadotomador = "SP";
                 NFeSIR.nfd.paistomador = "Brasil";
-                NFeSIR.nfd.fonetomador = 21119898;
+                NFeSIR.nfd.fonetomador = "21119898";
                 NFeSIR.nfd.faxtomador = "";
-                NFeSIR.nfd.ceptomador = 79010100;
+                NFeSIR.nfd.ceptomador = "79010100";
                 NFeSIR.nfd.bairrotomador = "Centro";
                 NFeSIR.nfd.emailtomador = "teste@smarapd.com.br";
-                NFeSIR.nfd.tppessoa = "J";
-                NFeSIR.nfd.cpfcnpjtomador = 30669959085741;
+                NFeSIR.nfd.cpfcnpjtomador = "30669959085741";
                 NFeSIR.nfd.inscricaoestadualtomador = "";
                 NFeSIR.nfd.inscricaomunicipaltomador = "";
                 NFeSIR.nfd.tbfatura = new tbnfdNfdFatura[3];
                 NFeSIR.nfd.tbfatura[0] = new tbnfdNfdFatura
                 {
-                    numfatura = "10/2018",
+                    numfatura = 1,
                     vencimentofatura = DateTime.Now.AddMonths(1).ToString("dd/MM/yyyy"),
-                    valorfatura = 100                   
+                    valorfatura = "100"                 
                 };
                 NFeSIR.nfd.tbfatura[1] = new tbnfdNfdFatura
                 {
-                    numfatura = "11/2018",
+                    numfatura = 2,
                     vencimentofatura = DateTime.Now.AddMonths(2).ToString("dd/MM/yyyy"),
-                    valorfatura = 100
+                    valorfatura = "100"
                 };
                 NFeSIR.nfd.tbfatura[2] = new tbnfdNfdFatura
                 {
-                    numfatura = "12/2018",
+                    numfatura =3,
                     vencimentofatura = DateTime.Now.AddMonths(3).ToString("dd/MM/yyyy"),
-                    valorfatura = 100
+                    valorfatura = "100"
                 };
                 NFeSIR.nfd.tbservico = new tbnfdNfdServico[3];
                 NFeSIR.nfd.tbservico[0] = new tbnfdNfdServico
                 {
-                    quantidade = 2,
+                    quantidade = "2",
                     descricao = "Serviços de Criação de Logomarca",
                     codatividade = 0101,
-                    valorunitario = 150,
+                    valorunitario = "150",
                     aliquota = "5.5",
                     impostoretido = "False"
                 };
                 NFeSIR.nfd.tbservico[1] = new tbnfdNfdServico
                 {
-                    quantidade = 1,
+                    quantidade = "1",
                     descricao = "Serviços de Criação de Logomarca",
                     codatividade = 0101,
-                    valorunitario = 200,
+                    valorunitario = "200",
                     aliquota = "5.5",
                     impostoretido = "False"
                 };
                 NFeSIR.nfd.tbservico[2] = new tbnfdNfdServico
                 {
-                    quantidade = 5,
+                    quantidade = "5",
                     descricao = "Serviços de Criação de Logomarca",
                     codatividade = 0101,
-                    valorunitario = 150,
+                    valorunitario = "150",
                     aliquota = "5.5",
                     impostoretido = "False"
                 };
@@ -90,16 +82,15 @@ namespace UnclePhill.WebAPI_NFeS.Domain
                 NFeSIR.nfd.razaotransportadora = "";
                 NFeSIR.nfd.cpfcnpjtransportadora = "";
                 NFeSIR.nfd.enderecotransportadora = "";
-                NFeSIR.nfd.pis = 0;
-                NFeSIR.nfd.cofins = 0;
-                NFeSIR.nfd.csll = 0;
-                NFeSIR.nfd.irrf = 0;
-                NFeSIR.nfd.inss = 0;
+                NFeSIR.nfd.pis = "0";
+                NFeSIR.nfd.cofins = "0";
+                NFeSIR.nfd.csll = "0";
+                NFeSIR.nfd.irrf = "0";
+                NFeSIR.nfd.inss = "0";
                 NFeSIR.nfd.descdeducoesconstrucao = "";
                 NFeSIR.nfd.totaldeducoesconstrucao = "";
-                NFeSIR.nfd.tributadonomunicipio = true;
+                NFeSIR.nfd.tributadonomunicipio = "true";
                 NFeSIR.nfd.numerort = "";
-                NFeSIR.nfd.fatorgerador = DateTime.Now.Month.ToString() + "/" + DateTime.Now.Year.ToString();
                 
                 WSEntrada wsEntrada = new WSEntradaClient();
                 nfdEntradaRequest nfdEntradaRequest = new nfdEntradaRequest();
