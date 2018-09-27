8a3a530d81ccb727f300c1bf150094a1a42cd5e1
Everaldo Cardoso de Araujo <everaldocardoso.github@gmail.com>
2018-09-27 17:58:22 -0300

    Assinatura

--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Utils/Utils/Signature.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Utils/Utils/Signature.cs
@@ -36,35 +36,32 @@ namespace UnclePhill.WebAPI_NFeS.Utils.Utils
 
     public class AssinaturaDigital
     {
-
-        public int SignXml(string XMLString, string RefUri, X509Certificate2 X509Cert)
         /*
-        * Entradas:
-        * XMLString: string XML a ser assinada
-        * RefUri : Referência da URI a ser assinada (Ex. infNFe
-        * X509Cert : certificado digital a ser utilizado na assinatura digital
-        *
-        * Retornos:
-        * Assinar : 0 - Assinatura realizada com sucesso
-        * 1 - Erro: Problema ao acessar o certificado digital - %exceção%
-        * 2 - Problemas no certificado digital
-        * 3 - XML mal formado + exceção
-        * 4 - A tag de assinatura %RefUri% inexiste
-        * 5 - A tag de assinatura %RefUri% não é unica
-        * 6 - Erro Ao assinar o documento - ID deve ser string %RefUri(Atributo)%
-        * 7 - Erro: Ao assinar o documento - %exceção%
-        *
-        * XMLStringAssinado : string XML assinada
-        *
-        * XMLDocAssinado : XMLDocument do XML assinado
-        */
+                * Entradas:
+                * XMLString: string XML a ser assinada
+                * RefUri : Referência da URI a ser assinada (Ex. infNFe
+                * X509Cert : certificado digital a ser utilizado na assinatura digital
+                *
+                * Retornos:
+                * Assinar : 0 - Assinatura realizada com sucesso
+                * 1 - Erro: Problema ao acessar o certificado digital - %exceção%
+                * 2 - Problemas no certificado digital
+                * 3 - XML mal formado + exceção
+                * 4 - A tag de assinatura %RefUri% inexiste
+                * 5 - A tag de assinatura %RefUri% não é unica
+                * 6 - Erro Ao assinar o documento - ID deve ser string %RefUri(Atributo)%
+                * 7 - Erro: Ao assinar o documento - %exceção%
+                *
+                * XMLStringAssinado : string XML assinada
+                *
+                * XMLDocAssinado : XMLDocument do XML assinado
+                */
+        public int SignXml(string XMLString, string RefUri, X509Certificate2 X509Cert)
         {
             int resultado = 0;
             msgResultado = "Assinatura realizada com sucesso";
             try
             {
-                // certificado para ser utilizado na assinatura
-                //
                 string _xnome = "";
                 if (X509Cert != null)
                 {
@@ -82,98 +79,54 @@ namespace UnclePhill.WebAPI_NFeS.Utils.Utils
                 }
                 else
                 {
-                    // certificado ok
                     _X509Cert = collection1[0];
-                    string x;
-                    x = _X509Cert.GetKeyAlgorithm().ToString();
-                    // Create a new XML document.
+                    string x = _X509Cert.GetKeyAlgorithm().ToString();
                     XmlDocument doc = new XmlDocument();
-
-                    // Format the document to ignore white spaces.
                     doc.PreserveWhitespace = false;
-
-                    // Load the passed XML file using it’s name.
                     try
                     {
                         doc.LoadXml(XMLString);
-
-                        // Verifica se a tag a ser assinada existe é única
                         int qtdeRefUri = doc.GetElementsByTagName(RefUri).Count;
 
                         if (qtdeRefUri == 0)
                         {
-                            // a URI indicada não existe
                             resultado = 4;
                             msgResultado = "A tag de assinatura " + RefUri.Trim() + " inexiste";
                         }
-                        // Exsiste mais de uma tag a ser assinada
                         else
                         {
                             if (qtdeRefUri > 1)
                             {
-                                // existe mais de uma URI indicada
                                 resultado = 5;
                                 msgResultado = "A tag de assinatura " + RefUri.Trim() + " não é unica";
 
                             }
-                            //else if (_listaNum.IndexOf(doc.GetElementsByTagName(RefUri).Item(0).Attributes.ToString().Substring(1,1))>0)
-                            //{
-                            // resultado = 6;
-                            // msgResultado = "Erro: Ao assinar o documento - ID deve ser string (" + doc.GetElementsByTagName(RefUri).Item(0).Attributes + ")";
-                            //}
                             else
                             {
                                 try
                                 {
 
-                                    // Create a SignedXml object.
                                     SignedXml signedXml = new SignedXml(doc);
-
-                                    // Add the key to the SignedXml document
                                     signedXml.SigningKey = _X509Cert.PrivateKey;
-
-                                    // Create a reference to be signed
-                                    Reference reference = new Reference();
-                                    
-                                    // pega o uri que deve ser assinada
-                                    //XmlAttributeCollection _Uri = doc.GetElementsByTagName(RefUri).Item(0).Attributes;
-                                    //foreach (XmlAttribute _atributo in _Uri)
-                                    //{
-                                    //    if (_atributo.Name == "Id")
-                                    //    {
-                                    //        reference.Uri = "#" + _atributo.InnerText;
-                                    //    }
-                                    //}
-
+                                    Reference reference = new Reference();                                    
                                     reference.Uri = "";
 
-                                    // Add an enveloped transformation to the reference.
                                     XmlDsigEnvelopedSignatureTransform env = new XmlDsigEnvelopedSignatureTransform();
                                     reference.AddTransform(env);
 
                                     XmlDsigC14NTransform c14 = new XmlDsigC14NTransform();
                                     reference.AddTransform(c14);
 
-                                    // Add the reference to the SignedXml object.
                                     signedXml.AddReference(reference);
 
-                                    // Create a new KeyInfo object
                                     KeyInfo keyInfo = new KeyInfo();
-
-                                    // Load the certificate into a KeyInfoX509Data object
-                                    // and add it to the KeyInfo object.
                                     keyInfo.AddClause(new KeyInfoX509Data(_X509Cert));
-
-                                    // Add the KeyInfo object to the SignedXml object.
                                     signedXml.KeyInfo = keyInfo;
 
                                     signedXml.ComputeSignature();
 
-                                    // Get the XML representation of the signature and save
-                                    // it to an XmlElement object.
                                     XmlElement xmlDigitalSignature = signedXml.GetXml();
 
-                                    // Append the element to the XML document.
                                     doc.DocumentElement.AppendChild(doc.ImportNode(xmlDigitalSignature, true));
                                     XMLDoc = new XmlDocument();
                                     XMLDoc.PreserveWhitespace = false;
