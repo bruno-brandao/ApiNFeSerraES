9fe18a7278b19018851a4d733de76105853d2852
Everaldo Cardoso de Araujo <everaldocardoso.github@gmail.com>
2018-10-05 17:48:15 -0300

    Incluindo a função para fazer upload do certificado digital

---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Domain/Domain/CertificatesDomain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnclePhill.WebAPI_NFeS.Models.Models;
+using UnclePhill.WebAPI_NFeS.Utils.Utils;
+
+namespace UnclePhill.WebAPI_NFeS.Domain.Domain
+{
+    public class CertificatesDomain: MasterDomain
+    {
+        public Certificates UploadCertificate(Certificates Certificate)
+        {
+            try
+            {
+                Validate(Certificate);
+
+                SQL = new StringBuilder();
+                SQL.AppendLine(" Insert Into Certificates ");
+                SQL.AppendLine(" ( CompanyId , ");
+                SQL.AppendLine("   Certificate , ");
+                SQL.AppendLine("   Password , ");
+                SQL.AppendLine("   Active , ");
+                SQL.AppendLine("   DateInsert , ");
+                SQL.AppendLine("   DateUpdate) ");
+                SQL.AppendLine(" Values ");
+                SQL.AppendLine(" ( " + Certificate.CompanyId);
+                SQL.AppendLine("  '" + Functions.NotQuote(Certificate.Certificate) + "',");
+                SQL.AppendLine("  '" + Functions.NotQuote(Certificate.Password) + "',");
+                SQL.AppendLine("  1, ");
+                SQL.AppendLine("  GetDate(), ");
+                SQL.AppendLine("  GetDate() ");
+                SQL.AppendLine(" ) ");
+
+                Certificate.CertificateId = Functions.Conn.Insert(SQL.ToString());
+                if (Certificate.CertificateId > 0)
+                {
+                    Certificate.Active = true;
+                    Certificate.DateInsert = DateTime.Now.ToString("yyyy-MM-dd");
+                    Certificate.DateUpdate = DateTime.Now.ToString("yyyy-MM-dd");
+                    return Certificate;
+                }
+                throw new Exception("Não foi possivel cadastrar o certificado!");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }            
+        }
+
+        private void Validate(Certificates Certificate)
+        {
+            if (Certificate.CompanyId <= 0)
+            {
+                throw new Exception("Informe a empresa!");
+            }
+
+            if (String.IsNullOrEmpty(Certificate.Certificate))
+            {
+                throw new Exception("Informe o certificado!");
+            }
+
+            if (string.IsNullOrEmpty(Certificate.Password))
+            {
+                throw new Exception("Informe a senha do certificado!");
+            }
+        }
+    }
+}
---
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Models/Models/Certificates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnclePhill.WebAPI_NFeS.Models.Models
+{
+    public class Certificates
+    {
+        public Certificates()
+        {
+
+        }
+
+        public Certificates(long CertificateId, long CompanyId, string Certificate, 
+            string Password, bool Active, string DateInsert, string DateUpdate)
+        {
+            this.CertificateId = CertificateId;
+            this.CompanyId = CompanyId;
+            this.Certificate = Certificate;
+            this.Password = Password;
+            this.Active = Active;
+            this.DateInsert = DateInsert;
+            this.DateUpdate = DateUpdate;
+        }
+
+        public long CertificateId { get; set; }
+        public long CompanyId { get; set; }
+        public string Certificate { get; set; }
+        public string Password { get; set; }
+        public bool Active { get; set; }
+        public string DateInsert { get; set; }
+        public string DateUpdate { get; set; }
+    }
+}
