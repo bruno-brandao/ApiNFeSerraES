85eac57034d30d0891397701427787e8534217b0
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2018-10-16 19:53:03 -0300

    Em meia hora vc faz as horas

--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Controllers/ResponsesController.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.API/Controllers/ResponsesController.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using System.Web.Mvc;
+using UnclePhill.WebAPI_NFeS.Domain.Domain;
 using UnclePhill.WebAPI_NFeS.Models.Models;
 
 namespace UnclePhill.WebAPI_NFeS.API.Controllers
@@ -13,6 +14,8 @@ namespace UnclePhill.WebAPI_NFeS.API.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ResponsesController : MasterController, Default.IController<Responses>
     {
+        private ResponsesDomain responsesDomain = new ResponsesDomain();
+
         [ApiExplorerSettings(IgnoreApi = true)]
         [System.Web.Http.ActionName("Get")]
         public IHttpActionResult Get(long Id = 0)
@@ -23,7 +26,17 @@ namespace UnclePhill.WebAPI_NFeS.API.Controllers
         [System.Web.Http.ActionName("Post")]
         public IHttpActionResult Post([FromBody] Responses obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (responsesDomain.Post(obj))
+                {
+                    return Ok();
+                }
+                return BadRequest("Não foi possivel registrar a resposta!");
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [ApiExplorerSettings(IgnoreApi = true)]
--- UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Domain/Domain/ResponsesDomain.cs
+++ UnclePhill.WebAPI_NFeS.API/UnclePhill.WebAPI_NFeS.Domain/Domain/ResponsesDomain.cs
@@ -3,15 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnclePhill.WebAPI_NFeS.Models.Models;
+using UnclePhill.WebAPI_NFeS.Utils.Utils;
 
 namespace UnclePhill.WebAPI_NFeS.Domain.Domain
 {
-    class ResponsesDomain : DefaultDomains.MasterDomain
+    public class ResponsesDomain : DefaultDomains.MasterDomain
     {
-
-        public void Post()
+        public bool Post(Responses responses)
         {
+            try
+            {
+                Validate(responses);
 
+                SQL = new StringBuilder();
+                SQL.AppendLine(" Insert Into Responses ");
+                SQL.AppendLine("   (UserId, ");
+                SQL.AppendLine("    QuestionId, ");
+                SQL.AppendLine("    OptionId, ");
+                SQL.AppendLine("    Correct, ");
+                SQL.AppendLine("    Active, ");
+                SQL.AppendLine("    DateInsert, ");
+                SQL.AppendLine("    DateUpdate ");
+                SQL.AppendLine("    ) ");
+                SQL.AppendLine(" Values ");
+                SQL.AppendLine("   (" + responses.UserId.ToString() + ",");
+                SQL.AppendLine("    " + responses.QuestionId.ToString() + ",");
+                SQL.AppendLine("    " + responses.OptionId.ToString() + ",");
+                SQL.AppendLine("    " + (responses.Correct? 1:0) + ",");
+                SQL.AppendLine("   1 ,");
+                SQL.AppendLine("   GetDate(), ");
+                SQL.AppendLine("   GetDate(), ");
+                SQL.AppendLine("   ) ");
+
+                if (Functions.Conn.Insert(SQL.ToString()) > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Validate(Responses responses)
+        {
+            if (responses.UserId <= 0)
+            {
+                throw new Exception("Informe o usuário!");
+            }
+
+            if (responses.QuestionId <= 0)
+            {
+                throw new Exception("Informe a questão!");
+            }
+
+            if (responses.OptionId <= 0)
+            {
+                throw new Exception("Informe a opção!");
+            }
         }
     }
 }
